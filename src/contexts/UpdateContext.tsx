0b9d8c1dfe7c9eaf8a5d5cc4a7e2b6a78b0e65c2
Roger Anacleto <rogerdasilvacode7@gmail.com>
2025-12-10 23:11:52 -0300

    feat: context update

---
+++ src/contexts/UpdateContext.tsx
@@ -0,0 +1,27 @@
+import { createContext, type ReactNode } from "react";
+import { useState } from "react";
+import { type RegisterProps } from "../components/TableProducts";
+
+interface UpdateProps{
+    products: RegisterProps | null;
+    changeUpdateModal: (register: RegisterProps) => void;
+}
+interface ChildrenProps{
+    children: ReactNode
+}
+
+export const UpdateContext = createContext({} as UpdateProps)
+
+const UpdateProvider = ({children}: ChildrenProps) => {
+const [products, setProducts] = useState<RegisterProps | null>(null)
+
+const changeUpdateModal = (register: RegisterProps) => {
+    setProducts(register)
+}
+    return(
+        <UpdateContext.Provider value={{changeUpdateModal, products}}>
+            {children}
+        </UpdateContext.Provider>
+    )
+}
+export { UpdateProvider };
